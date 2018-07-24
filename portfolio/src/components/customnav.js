52d4218624180bf10382cb61187451245bd06b96
CHICHEN\Chichen <Rosales.G793036@stu.sanjac.edu>
2018-07-23 21:47:17 -0500

    Changes made
    Changes to CSS to limit div sizes

--- portfolio/src/components/customnav.js
+++ portfolio/src/components/customnav.js
@@ -9,24 +9,31 @@ class customnav extends Component {
   render() {
     return (
       <HashRouter>
-        <div className="nav">
-          <ul className="menu">
-            <li>
-              <NavLink className="menu-item" to="/portfolio">
-                Portfolio
+        <div>
+          <nav className="stroke">
+            <ul className="menu">
+              <li>
+                <NavLink className="menu-item" to="/">
+                  Home
               </NavLink>
-            </li>
-            <li>
-              <NavLink className="menu-item" to="/contact">
-                Contact
+              </li>
+              <li>
+                <NavLink className="menu-item" to="/portfolio">
+                  Portfolio
               </NavLink>
-            </li>
-          </ul>
+              </li>
+              <li>
+                <NavLink className="menu-item" to="/contact">
+                  Contact
+              </NavLink>
+              </li>
+            </ul>
 
-          <Route exact path="/" component={Index} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route path="/contact" component={Contact} />
+            <Route exact path="/" component={Index} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
 
+          </nav>
         </div>
       </HashRouter>
     );
