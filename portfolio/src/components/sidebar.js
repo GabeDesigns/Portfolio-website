cf9060399870ccb8452078f92be251bba9f6030c
CHICHEN\Chichen <Rosales.G793036@stu.sanjac.edu>
2018-07-25 22:22:43 -0500

    new changes

---
+++ portfolio/src/components/sidebar.js
@@ -0,0 +1,23 @@
+import { stack as Menu1 } from 'react-burger-menu';
+import { elastic as Menu2 } from 'react-burger-menu';
+import { bubble as Menu3 } from 'react-burger-menu';
+import { push as Menu4 } from 'react-burger-menu';
+import { pushRotate as Menu5 } from 'react-burger-menu';
+import { scaleDown as Menu6 } from 'react-burger-menu';
+import { scaleRotate as Menu7 } from 'react-burger-menu';
+import { fallDown as Menu8 } from 'react-burger-menu';
+import { reveal as Menu9 } from 'react-burger-menu';
+
+const Menus = [Menu1, Menu2, Menu3, Menu4, Menu5, Menu6, Menu7, Menu8, Menu9];
+
+const Menu = Menus[Math.floor(Math.random() * Menus.length)];
+
+
+
+
+export default Menu;
+
+
+
+
+
