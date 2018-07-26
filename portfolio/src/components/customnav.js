96c15ecfda77fdae0de7f43241f9588761e5f71f
CHICHEN\Chichen <Rosales.G793036@stu.sanjac.edu>
2018-07-26 15:10:18 -0500

    Revert "new changes"
    This reverts commit cf9060399870ccb8452078f92be251bba9f6030c.

--- portfolio/src/components/customnav.js
+++ portfolio/src/components/customnav.js
@@ -1,7 +1,5 @@
 import React, { Component } from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
-//import { stack as Menu } from 'react-burger-menu';
-import Menu from './sidebar';
 import Contact from "../pages/contact";
 import Portfolio from "../pages/portfolio";
 import Index from "../pages/index";
@@ -10,35 +8,33 @@ import "../styles/customnav.css";
 class customnav extends Component {
   render() {
     return (
-      <Menu pageWrapId={"page-wrap"} outerContainerId={"outer-container"} right>
-        <HashRouter>
-          <div>
-            <nav className="stroke">
-              <ul className="menu">
-                <li>
-                  <NavLink className="menu-item" to="/">
-                    Home
+      <HashRouter>
+        <div>
+          <nav className="stroke">
+            <ul className="menu">
+              <li>
+                <NavLink className="menu-item" to="/">
+                  Home
                 </NavLink>
-                </li>
-                <li>
-                  <NavLink className="menu-item" to="/portfolio">
-                    Portfolio
+              </li>
+              <li>
+                <NavLink className="menu-item" to="/portfolio">
+                  Portfolio
                 </NavLink>
-                </li>
-                <li>
-                  <NavLink className="menu-item" to="/contact">
-                    Contact
+              </li>
+              <li>
+                <NavLink className="menu-item" to="/contact">
+                  Contact
                 </NavLink>
-                </li>
-              </ul>
+              </li>
+            </ul>
 
-              <Route exact path="/" component={Index} />
-              <Route path="/portfolio" component={Portfolio} />
-              <Route path="/contact" component={Contact} />
-            </nav>
-          </div>
-        </HashRouter>
-      </Menu>
+            <Route exact path="/" component={Index} />
+            <Route path="/portfolio" component={Portfolio} />
+            <Route path="/contact" component={Contact} />
+          </nav>
+        </div>
+      </HashRouter>
     );
   }
 }
