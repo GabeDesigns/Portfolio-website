7f6793a0fb9fce54d4d1f86d82db1ccf9441a6f9
Gabriel Rosales <gabriel.rosales0129@gmail.com>
2018-11-14 19:31:00 -0600

    new changes
    New changes

--- portfolio/src/pages/about.js
+++ portfolio/src/pages/about.js
@@ -9,8 +9,8 @@ export class index extends Component {
                 <fieldset className="box">
                     <legend className="header">About Me</legend>
                     <p>I've always been fascinated with computers and anything related to science. From a young age I began tinkering with computers, seeing how they work, and fixing them.
-                    My passion grew more in highschool where I began to learn what programming was and how to code. C# was the first language I ever learned with
-                    <span className="tech">Java , Javascript , HTML , CSS, Node.js , React.js</span><span className="highlight">( This website was made with it! )</span>.
+                    My passion grew more in high school where I began to learn what programming was and how to code. C# was the first language I ever learned with
+                    <span className="tech"> Java , Javascript , HTML , CSS, Node.js , React.js</span><span className="highlight">( This website was made with it! )</span>.
                                 From High School I worked a few IT related jobs, but my pasion is coding.
                                 I hope to spread my passion for coding and lead a team of developers as a Lead Full Stack Web Dev.
                 </p>
